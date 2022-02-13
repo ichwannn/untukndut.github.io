c84b8c4600125006b042b3ade9367f396df7f696
ichwannn <ichwansanusi101@gmail.com>
2022-02-14 01:35:23 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+alert('Selamat Datang');
+var lagi = false;
+
+while (lagi === false) {
+  var nama = prompt('Dengan siapa nihh, namanya siapa??');
+  alert('halo ' + nama);
+  alert('Ada sesuatu nihh buat kamu..');
+
+  lagi = confirm('Mau tau gak?');
+}
+alert('Mau tau banget?');
+alert('Mau tau banget, apa mau tau aja??');
+alert('Yaudah dehh, nihh aku tunjukkin..');
+alert('Maaf yaa, cuma ini yang aku kasih hehehe');
+alert('Semoga seneng yaa :)');
